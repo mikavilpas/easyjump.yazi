2dc519c022ef54e3a25c7bfaaee0944051d0a4f5
Mika Vilpas <mika.vilpas@gmail.com>
2025-08-23 21:47:58 +0300

    test!: reorganize the project and add end-to-end tests (#2)

---
+++ integration-tests/cypress.config.ts
@@ -0,0 +1,65 @@
+import { defineConfig } from "cypress"
+import fs from "fs"
+import { readFile, rm } from "fs/promises"
+import path from "path"
+import { fileURLToPath } from "url"
+import { inspect } from "util"
+
+const __dirname = fileURLToPath(new URL(".", import.meta.resolve(".")))
+
+const yaziLogFile = path.resolve(__dirname, "test-environment/.repro/yazi.log")
+
+console.log(`yaziLogFile: ${yaziLogFile}`)
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    video: true,
+    setupNodeEvents(on, _config) {
+      on("after:spec", (_spec, results): void => {
+        // https://docs.cypress.io/app/guides/screenshots-and-videos#Delete-videos-for-specs-without-failing-or-retried-tests
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) => {
+            return test.attempts.some((attempt) => attempt.state === "failed")
+          })
+          if (!failures && fs.existsSync(results.video)) {
+            // delete the video if the spec passed and no tests retried
+            return fs.unlinkSync(results.video)
+          }
+        }
+      })
+
+      on("task", {
+        async removeYaziLog(): Promise<null> {
+          try {
+            await rm(yaziLogFile)
+          } catch (err) {
+            if (err.code !== "ENOENT") {
+              console.error(err)
+            }
+          }
+          return null // something must be returned
+        },
+        async showYaziLog(): Promise<null> {
+          try {
+            const log = await readFile(yaziLogFile, "utf-8")
+            console.log(
+              `${yaziLogFile}`,
+              inspect(log.split("\n"), { maxArrayLength: null, colors: true }),
+            )
+            return null
+          } catch (err) {
+            console.error(err)
+            return null // something must be returned
+          }
+        },
+      })
+    },
+    experimentalRunAllSpecs: true,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+  },
+})
---
+++ integration-tests/cypress/support/e2e.ts
@@ -0,0 +1,24 @@
+// ***********************************************************
+// This example support/e2e.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import "./tui-sandbox.ts"
+
+before(function () {
+  // disable Cypress's default behavior of logging all XMLHttpRequests and
+  // fetches to the Command Log
+  // https://gist.github.com/simenbrekken/3d2248f9e50c1143bf9dbe02e67f5399?permalink_comment_id=4615046#gistcomment-4615046
+  cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
+})
