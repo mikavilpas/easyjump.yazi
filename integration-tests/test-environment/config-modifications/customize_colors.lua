ee50981353aa39d7e45057df1da41657e6b9d194
Mika Vilpas <mika.vilpas@gmail.com>
2025-08-30 09:34:00 +0300

    test: simplify config-modifications/customize_colors.lua

--- integration-tests/test-environment/config-modifications/customize_colors.lua
+++ integration-tests/test-environment/config-modifications/customize_colors.lua
@@ -1,6 +1,6 @@
-local opts = {
+ya.dbg("customizing easyjump colors")
+
+require("easyjump"):setup({
   icon_fg = "#94e2d5",
   first_key_fg = "#45475a",
-}
-ya.dbg("customizing easyjump colors", opts)
-require("easyjump"):setup(opts)
+})
